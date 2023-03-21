ba7ccdb3c78e86a6087c3b9018a4171c75752bdb
valentin_bazdukov <bazdukovvalentin@gmail.com>
2023-03-21 10:45:27 +1000

    fix(application): Исправить контроллер приложения
    QtCharts не поддерживает QGuiApplication

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
